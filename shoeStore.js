f92fa87a40a21570b69774a5ccac8e6d171e972a
Diana Tineo <diana.tineo1@gmail.com>
2021-10-01 19:09:29 -0400

    get orderbyid2

--- shoeStore.js
+++ shoeStore.js
@@ -21,8 +21,20 @@ const getCustomerCollection = async () => {
 }
 
 const createCustomer = async ({name, lastName, address, email})=>{
-  const userCollection = await getCustomerCollection()
-  const ret = await userCollection.insertOne({name, lastName, address, email})
+  const customerCollection = await getCustomerCollection()
+  const ret = await customerCollection.insertOne({name, lastName, address, email})
+  return ret.insertedId
+}
+
+const getOrdersCollection = async () =>{
+  const client = await createClient()
+  const db = client.db('ShoeStoreDB')
+  return db.collection('orders')
+}
+
+const createOrders = async ({customer, shoes, date}) =>{
+  const orders = await getOrdersCollection()
+  const ret = await orders.insertOne({customer, shoes, date})
   return ret.insertedId
 }
 
@@ -32,13 +44,17 @@ const getShoesCollection = async () =>{
   return db.collection('shoes')
 }
 
-const createShoes = ({type, color, size, price}) =>{
-  const shoeCollection = await getShoesCollection()
-  const ret = await shoeCollection.insertOne({type, color, size, price})
+const createShoes = async ({type, color, size, price}) => {
+  const shoesCollection = await getShoesCollection()
+  const ret = await shoesCollection.insertOne({type, color, size, price})
   return ret.insertedId
 }
 
-
+const getOrderById = async (id) =>{
+  const orderCollection = await orderCollection()
+  const ret = await orderCollection.findOne(id)
+  return ret
+}
 
 const run = async () => {
   const client = await createClient()
@@ -48,13 +64,23 @@ const run = async () => {
     address: faker.address.streetAddress(),
     email: faker.internet.email()
   })
+
   let shoeId = await createShoes({
-    type: faker.shoes.type(),
+    type: faker.commerce.productMaterial(),
     color: faker.commerce.color(),
     size: faker.datatype.number(12),
     price: faker.commerce.price()
   })
-  console.log(customerId)
+ 
+  await createOrders({
+    customer: customerId,
+    shoes: shoeId,
+    date: faker.datatype.datetime()
+  })
+
+  let orders = await getOrderById()
+  console.log(orders)
+
   await client.close()
 }
  run().then()
